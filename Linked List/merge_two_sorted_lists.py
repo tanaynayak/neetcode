74c70969a99d913ddb827339cb4c392036918c40
tanaynayak <tanay.nayak@gmail.com>
2023-12-02 20:44:02 -0500

    Updated code

--- Linked List/merge_two_sorted_lists.py
+++ Linked List/merge_two_sorted_lists.py
@@ -10,16 +10,16 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        # if list1 is None:
-        #     return list2
-        # if list2 is None:
-        #     return list1
-        # if list1.val <= list2.val:
-        #     list1.next = self.mergeTwoLists(list1.next, list2)
-        #     return list1
-        # else:
-        #     list2.next = self.mergeTwoLists(list1, list2.next)
-        #     return list2
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
         dummy = ListNode()
         tail = dummy
         while list1 and list2:
--- Linked List/reverse_linked_list.py
+++ Linked List/reverse_linked_list.py
@@ -9,12 +9,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # if head is None or head.next is None:
-        #     return head
-        # new_head = self.reverseList(head.next)
-        # head.next.next = head
-        # head.next = None
-        # return new_head
+        if head is None or head.next is None:
+            return head
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
         prev = None
         curr = head
         while curr:
